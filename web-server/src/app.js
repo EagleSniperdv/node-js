d57908faae6d4d2aa55c532783c646fd76647444
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-02-23 15:23:12 +0300

    serving-front-end

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,19 @@
+const path = require('path')
 const express = require('express')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public/index.html'))
+
 
 const app = express()
 
-app.get('',(req,res) => {
-    res.send('<h1>Header</h1>')
-})
+const publicDir = path.join(__dirname, '../public')
+const helpDir = path.join(__dirname,'../public/help.html')
+const aboutDir = path.join(__dirname,'../public/about.html')
 
-app.get('/help',(req,res) => {
-    res.send({
-        name:'Naboth',
-        age: 24
-    })
-})
+app.use(express.static(publicDir))
 
-app.get('/about',(req,res) => {
-    res.send('about page')
-})
+
 
 app.get('/weather',(req,res) => {
     res.send('weather page')
