d266985563bc236bede10ec603346a75b60c9769
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-03-01 19:53:36 +0300

    delete

--- task-app/src/app.js
+++ task-app/src/app.js
@@ -40,6 +40,10 @@ app.get('/users/:id', async (req,res) => {
     try {
         const { id } = req.params
         const user = await User.findById(id)
+
+        if(!user) {
+            return res.status(404).json({message: "Page notFound"})
+        }
         res.status(200).json(user)
     } catch (error) {
         res.status(500).json({message: error.message})
@@ -66,6 +70,61 @@ app.get('/task/:id',async (req,res) => {
         res.status(500).json({message: error.message})
     }
 })
+
+app.put('/users/:id', async (req,res) => {
+    try {
+        const {id} = req.params
+        const user = await User.findByIdAndUpdate(id, req.body)
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/task/:id', async (req,res) => {
+    try {
+        const {id} = req.params
+        const task = await Task.findByIdAndUpdate(id, req.body)
+
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/task/:id', async (req,res) => {
+    try {
+        const { id } = req.params
+        const task = await Task.findByIdAndDelete(id)
+
+        if(!task) {
+            res.status(404).json({message: "not found"})
+        }
+
+        res.status(200).json({message: "task deleted successfully"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/users/:id', async (req,res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findByIdAndDelete(id)
+
+        if(!user) {
+            res.status(404).json({message: "not found"})
+        }
+
+
+        res.status(200).json("task deleted successfully")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
 
 
 mongoose.connect("mongodb+srv://nabothongwenyi40:7M9wTbZXcTxA2GvV@nodebackend.oitngul.mongodb.net/user-data?retryWrites=true&w=majority&appName=nodebackend")
