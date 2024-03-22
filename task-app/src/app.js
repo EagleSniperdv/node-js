19af61448d3d9c71622b9c4ef2ba62fd995454ed
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-03-22 10:53:11 +0300

    servig

--- task-app/src/app.js
+++ task-app/src/app.js
@@ -222,3 +222,30 @@ app.listen(3000,() => {
 // }
 
 // myFunc()
+const multer = require('multer')
+
+const upload = multer({
+    dest:'images',
+    limits:{
+        fileSize: 1000000
+    },
+    fileFilter(req,file,cb) {
+        if (
+            !file.originalname.match(/\.jpg$/)
+        ) {
+            return cb(new Error('Please Upload jpg img'))
+        }
+
+        cb(undefined,true)
+    }
+})
+
+// const errMiddleware = (req,res,next) => {
+//     throw new Error('From my middleware')
+// }
+
+app.post('/upload',upload.single('upload'),(req,res) => {
+    res.send()
+},(error,req,res,next) => {
+    res.status(400).send({error: error.message})
+})
