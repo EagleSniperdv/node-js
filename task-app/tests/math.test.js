513a7170359553fb136280f27a1a13934742cf91
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-03-28 15:33:52 +0300

    test

--- task-app/tests/math.test.js
+++ task-app/tests/math.test.js
@@ -6,13 +6,34 @@
 //     throw new Error
 // })
 
-const {calculateTip} = require('../src/math')
+const {
+    calculateTip,
+    celcFaren,
+    farenCelc
+    } = require('../src/math')
 
-test('total + tip',() => {
+test('total + tip test ',() => {
 
-    const total = calculateTip(10,.3)
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
 
-    if (total !== 13) {
-        throw new Error('wrong tip')
-    }
+    // if (total !== 13) {
+    //     throw new Error('wrong tip')
+    // }
+})
+
+test('calculate with default tipPercent',() => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
+test('calculate faren to celc',() => {
+    const temp = farenCelc(32)
+    expect(temp).toBe(0)
+})
+
+test('celc to faren', () => {
+    const temp = celcFaren(0)
+    expect(temp).toBe(32)
+
 })
